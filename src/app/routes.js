d651336938d7ab6afed48f33b7c08c652ace223a
Monir <monir.sust14@gmail.com>
2025-06-28 11:32:42 +0600

    added weather app privacy page

--- src/app/routes.js
+++ src/app/routes.js
@@ -5,6 +5,7 @@ import { Home } from "../pages/home";
 import { Portfolio } from "../pages/portfolio";
 import { ContactUs } from "../pages/contact";
 import { About } from "../pages/about";
+import { Privacy } from "../pages/privacy";
 import { Socialicons } from "../components/socialicons";
 import { CSSTransition, TransitionGroup } from "react-transition-group";
 
@@ -24,6 +25,7 @@ const AnimatedRoutes = withRouter(({ location }) => (
         <Route path="/about" element={<About />} />
         <Route path="/portfolio" element={<Portfolio />} />
         <Route path="/contact" element={<ContactUs />} />
+        <Route path="/privacy/banglar-weather" element={<Privacy />} />
         <Route path="*" element={<Home />} />
       </Routes>
     </CSSTransition>
